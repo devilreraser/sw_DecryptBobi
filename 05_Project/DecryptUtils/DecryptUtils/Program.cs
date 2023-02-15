2264c616f1453081e7b001774e9aa4950e522c4d
Dimitar Lilov <devilerser@gmail.com>
2023-02-15 18:54:54 +0200

    Hello World

---
+++ 05_Project/DecryptUtils/DecryptUtils/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DecryptUtils
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
