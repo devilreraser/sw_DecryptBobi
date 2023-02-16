a8f924d3d30eb3d1013813df684b811555fe8415
Dimitar Lilov <devilerser@gmail.com>
2023-02-16 12:47:44 +0200

    Still starting

--- 05_Project/DecryptUtils/DecryptUtils/Program.cs
+++ 05_Project/DecryptUtils/DecryptUtils/Program.cs
@@ -1,10 +1,77 @@
-﻿namespace DecryptUtils
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        // Verify that at least 1 input argument was provided
+        if (args.Length < 1)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Usage: program.exe encrypted_file [decrypted_file] [original_file]");
+            return;
         }
+
+        // Get the input arguments
+        string encryptedFilePath = args[0];
+        string decryptedFilePath = Path.ChangeExtension(encryptedFilePath, null) + ".decrypted";
+        if (args.Length > 1)
+        {
+            decryptedFilePath = args[1];
+        }
+        string? originalFilePath = null;
+        if (args.Length > 2)
+        {
+            originalFilePath = args[2];
+        }
+
+        // Read in the contents of the encrypted file
+        byte[] encryptedBytes = File.ReadAllBytes(encryptedFilePath);
+
+        // Decrypt the contents using a provided decryption algorithm (e.g. AES)
+        byte[] decryptedBytes;
+        using (Aes aes = Aes.Create())
+        {
+            // Use a hard-coded key and IV for demonstration purposes only; in practice, use a securely generated key and IV
+            byte[] key = { 0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c };
+            byte[] iv = { 0x8e, 0x3c, 0xf3, 0x03, 0x9b, 0x3d, 0x79, 0x2a, 0x0c, 0xb4, 0x5d, 0x03, 0x7e, 0x7d, 0x2e, 0x8d };
+            aes.Key = key;
+            aes.IV = iv;
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (CryptoStream cs = new CryptoStream(ms, aes.CreateDecryptor(), CryptoStreamMode.Write))
+                {
+                    cs.Write(encryptedBytes, 0, encryptedBytes.Length);
+                    cs.FlushFinalBlock();
+                }
+                decryptedBytes = ms.ToArray();
+            }
+        }
+
+        // Write the decrypted contents to the specified decrypted file
+        File.WriteAllBytes(decryptedFilePath, decryptedBytes);
+
+        // If an original file was specified, compare the decrypted contents to the contents of the original file to verify that the decryption was successful
+        if (originalFilePath != null)
+        {
+            byte[] originalBytes = File.ReadAllBytes(originalFilePath);
+            if (decryptedBytes.Length != originalBytes.Length)
+            {
+                Console.WriteLine("Decrypted file length does not match original file length.");
+                return;
+            }
+            for (int i = 0; i < originalBytes.Length; i++)
+            {
+                if (decryptedBytes[i] != originalBytes[i])
+                {
+                    Console.WriteLine("Decrypted file contents do not match original file contents.");
+                    return;
+                }
+            }
+        }
+
+        Console.WriteLine("Decryption successful.");
     }
 }
